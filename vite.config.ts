023893a4651f73d3fed6461abb2f53646c66b28e
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2026-03-31 11:17:03 -0700

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/calculator/',     // ← Change this if your repo name is different
+  base: '/calculator/',    
 })
